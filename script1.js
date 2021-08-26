8778fff7cd181876003db79bfd6420322cdd1034
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-08-26 18:13:17 -0300

    Primeiro exercicio

---
+++ script1.js
@@ -0,0 +1,12 @@
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for( let index = 0; index < numbers.length; index += 1){
+//   let number = numbers[index];
+//   console.log( number );
+// }
+
+
+for ( let number of numbers){
+  console.log(number);
+}
