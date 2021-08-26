d408d0655b3b5543daace49c97405059791af062
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-08-25 23:58:25 -0300

    Update scriptEx9.js

--- 01-fundamentos/bloco-04-introducao-java-script/scriptEx9.js
+++ 01-fundamentos/bloco-04-introducao-java-script/scriptEx9.js
@@ -1,3 +1,12 @@
 // Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
 // Bonus: use somente um if .
 
+const a = 10;
+const b = 554;
+const c = 91;
+
+if ( ( a % 2 === 1) || ( b % 2 === 1 ) || ( c % 2 === 1) ) {
+  console.log("tem impar");
+}else{
+  console.log("tudo par");
+}
