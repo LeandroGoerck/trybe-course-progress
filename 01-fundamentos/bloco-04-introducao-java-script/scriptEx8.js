8a374854d7f1a959f4006b1bcc4b0658820b771a
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-08-25 23:48:14 -0300

    Update scriptEx8.js

--- 01-fundamentos/bloco-04-introducao-java-script/scriptEx8.js
+++ 01-fundamentos/bloco-04-introducao-java-script/scriptEx8.js
@@ -1,3 +1,12 @@
 // Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
 // Bonus: use somente um if .
 
+const a = 100;
+const b = 55;
+const c = 9;
+
+if ( ( a % 2 === 0) || ( b % 2 ===0 ) || ( c % 2 === 0) ) {
+  console.log("tem par");
+}else{
+  console.log("tudo impar");
+}
