e98e0b739b88c784fc3893646f6606198baae880
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-08-25 23:40:47 -0300

    Update scriptEx7.js

--- 01-fundamentos/bloco-04-introducao-java-script/scriptEx7.js
+++ 01-fundamentos/bloco-04-introducao-java-script/scriptEx7.js
@@ -7,3 +7,20 @@
 // Porcentagem < 50 -> F
 // O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
 
+let nota = 85;
+
+if ( nota >= 90 ){
+  console.log("A");
+}else if( nota >= 80){
+  console.log("B");
+}else if( nota >= 70){
+  console.log("C");
+}else if( nota >= 60){
+  console.log("D");
+}else if( nota >= 50){
+  console.log("E");
+}else if ( nota < 50 ){
+  console.log("F");
+}else{
+  console.log("erro");
+}
