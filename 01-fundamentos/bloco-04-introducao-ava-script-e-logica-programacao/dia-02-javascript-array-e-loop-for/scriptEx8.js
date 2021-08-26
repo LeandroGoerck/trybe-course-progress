e6003cb91f8fcd5e509c4768d94864f7356fa549
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-08-26 18:53:42 -0300

    Create scriptEx8.js

---
+++ 01-fundamentos/bloco-04-introducao-ava-script-e-logica-programacao/dia-02-javascript-array-e-loop-for/scriptEx8.js
@@ -0,0 +1,11 @@
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let arrayVictim = [];
+
+for ( let i = 0; i < 25; i += 1 ){
+
+  arrayVictim.push( i + 1 );
+
+}
+
+console.log(arrayVictim);
