c72b1d21d40bbcc9b0fd29d1c31224f2a41321d7
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-08-26 18:30:15 -0300

    Update scriptEx3.js

--- 01-fundamentos/bloco-04-introducao-ava-script-e-logica-programacao/dia-02-javascript-array-e-loop-for/scriptEx3.js
+++ 01-fundamentos/bloco-04-introducao-ava-script-e-logica-programacao/dia-02-javascript-array-e-loop-for/scriptEx3.js
@@ -3,3 +3,13 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+let somatorio = 0;
+let media     = 0;
+
+for ( let index=0; index < numbers.length; index += 1){
+  somatorio = somatorio + numbers[ index ];
+}
+
+media = somatorio / numbers.length;
+
+console.log("A media dos valores do vetor é: " + media);
