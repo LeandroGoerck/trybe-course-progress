a668edc2a7132eac0c90293f03fcdef4d19add4e
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-08-30 21:19:43 -0300

    Exercicio Indice Max Value

--- 01-fundamentos/bloco-04-introducao-ava-script-e-logica-programacao/dia-04-java-script-objetos-e-funcoes/functions1.js
+++ 01-fundamentos/bloco-04-introducao-ava-script-e-logica-programacao/dia-04-java-script-objetos-e-funcoes/functions1.js
@@ -6,3 +6,29 @@
 // Retorno esperado: false
 
 
+function checkPalindome( phrase ) {
+  let phraseLength = phrase.length;
+  let equalCount = 0;
+  let incremetalPointer  = 0;
+  let decrementalPointer = phrase.length;
+
+  for( incremetalPointer = 0; incremetalPointer < phraseLength ; incremetalPointer += 1){
+    decrementalPointer = phraseLength-(incremetalPointer+1);
+    if ( phrase[incremetalPointer] === phrase[decrementalPointer]  ){
+      equalCount += 1 ;
+    }
+    //console.log( phrase[incremetalPointer], phrase[decrementalPointer] );
+  }
+  //console.log(equalCount);
+  if ( equalCount === phraseLength){
+    return true;
+  }
+  else{
+    return false;
+  }
+
+}
+
+console.log( checkPalindome("arara") );
+console.log( checkPalindome('desenvolvimento') );
+console.log( checkPalindome('oniino') );
