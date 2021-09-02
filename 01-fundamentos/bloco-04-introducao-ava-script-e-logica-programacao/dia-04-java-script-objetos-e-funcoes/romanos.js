4d69d5c3ed9cd121b854c5d37d6d679aeb14a1a4
Leandro Elias Goerck <lgoerck@universo.univates.br>
2021-09-02 18:38:45 -0300

    muitas coisas

---
+++ 01-fundamentos/bloco-04-introducao-ava-script-e-logica-programacao/dia-04-java-script-objetos-e-funcoes/romanos.js
@@ -0,0 +1,23 @@
+// 1 - (Difícil) Faça um programa que receba uma string em algarismos romanos e retorne o número que a string representa.
+
+let romanNumbers = {
+  'I' :    1,
+  'IV':    4,
+  'V' :    5,
+  'IX':    9,
+  'X' :   10,
+  'XL':   40,
+  'L' :   50,
+  'XC':   90,
+  'C' :  100,
+  'CD':  400,
+  'D' :  500,
+  'CM':  900,
+  'M' : 1000
+}
+
+function romanCalc( romanNumber ) {
+  let result = 0;
+  
+
+}
