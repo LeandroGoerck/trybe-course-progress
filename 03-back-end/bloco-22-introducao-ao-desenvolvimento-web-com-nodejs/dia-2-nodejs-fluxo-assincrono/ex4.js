4cba015c0c2b3f5358d1b25a1bd51b8f93b76ad8
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-03-15 15:18:24 -0300

    saving changes

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/ex4.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/ex4.js
@@ -9,19 +9,55 @@
 // Crie uma função que substitua o personagem Nelson Muntz pela personagem Maggie Simpson no arquivo simpsonFamily.json .
 
 const simpsons = require('./simpsons.json');
+const fs = require('fs').promises;
 
 function printAll() {
   simpsons.forEach((simpson) => console.log(`${simpson.id} ${simpson.name}`));
 }
 
-printAll();
+// printAll();
+
+
+function printSimpson(charId) {
+  return new Promise((resolve, reject) => {
+    if(charId > simpsons.length) return reject('Numero id muito alto');
+    if(charId <= 0) return reject('Numero id muito Baixo');
+
+    resolve(simpsons[charId-1].name);
+  })
+}
+
+printSimpson(1)
+   .then(result => console.log(result))
+   .catch(error => console.log(error));
 
 
 
+ async function getSimpsonById(id) {
+   const simpsons = await fs
+     .readFile('./simpsons.json', 'utf-8')
+     .then((fileContent) => JSON.parse(fileContent));
 
+   const chosenSimpson = simpsons.find((simpson) => simpson.id === id);
 
+   if (!chosenSimpson) {
+     /* A palavra-chave `throw` dispara um erro que deve ser tratado por quem chamou nossa função.
+      * Em funções `async`, utilizar `throw` faz com que a Promise seja rejeitada,
+      * tendo como motivo o que passarmos para o `throw`.
+      * Ou seja, a linha abaixo rejeita a Promise da nossa função com o motivo 'id não encontrado'
+      */
+     throw new Error('id não encontrado');
+   }
 
+     /* Da mesma forma que `throw` aciona o fluxo de erro e rejeita a Promise,
+      * `return` aciona o fluxo de sucesso e resolve a Promise.
+      * Sendo assim, a linha abaixo é equivalente a chamar `resolve(chosenSimpson)`
+      * dentro do executor de uma Promise.
+      */
+     return chosenSimpson;
+ }
 
+getSimpsonById('5');
 
 
 
