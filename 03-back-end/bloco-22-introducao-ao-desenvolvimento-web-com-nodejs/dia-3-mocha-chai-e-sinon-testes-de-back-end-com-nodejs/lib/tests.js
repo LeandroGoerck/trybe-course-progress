6bf07a7595041372363601c3228bf0ee16be0c70
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-03-16 15:19:50 -0300

    save last changes

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/lib/tests.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/lib/tests.js
@@ -1,5 +1,6 @@
 
 function describeNumbers(number) {
+  if (typeof number !== 'number') return 'the given value is not a number';
   if (number > 0) return 'positive';
   if (number < 0) return 'negative';
   if (number == 0) return 'neutral';
--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/tests.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/tests.js
@@ -4,8 +4,8 @@ const expect = require('chai').expect;
 
 
 describe('test numbers', () => {
+
   it('tests if 2 is positive', () => {
-    
     expect(describeNumbers(2)).to.be.equal('positive');
   });
 
@@ -15,6 +15,14 @@ describe('test numbers', () => {
 
   it('tests if 0 is neutral', () => {
     expect(describeNumbers(0)).to.be.equal('neutral');
+  });
+
+})
+
+describe('if the param is not a number', () => {
+
+  it('returns the massage: the given value is not a number', () => {
+    expect(describeNumbers('ABCD')).to.be.equal('the given value is not a number');
   });
 
 })
