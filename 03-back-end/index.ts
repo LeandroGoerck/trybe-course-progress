77c281036e4cb15b654f51d99e841d5b2cedd06c
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-05-17 15:35:48 -0300

    change day

---
+++ 03-back-end/index.ts
@@ -0,0 +1,39 @@
+
+enum DaysOfWeek {
+  Sunday = 1,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
+enum RainbowColors {
+  Red = "Vermelho",
+  Orange = "Laranja",
+  Yellow = "Amarelo",
+  Green = "Verde",
+  Blue = "Azul",
+  Indigo = "Anil",
+  Violet = "Violeta",
+}
+
+enum Actions {
+  save = 1,
+  print,
+  open,
+  visualize,
+  close,
+}
+
+enum CardinalPoints {
+  North = 'N',
+  Eest = 'E',
+  South = 'S', 
+  West = 'W',
+}
+
+
+
+
