fa2b3cf39b207120559e7cbf0301ee8530faec0d
Leandro Elias Goerck <lgoerck@universo.univates.br>
2022-06-23 20:23:13 -0300

    exercises 1 to 9 concluded

--- 03-back-end/bloco-29-introducao-ao-mongodb/dia-4-updates-simples/update-simples.js
+++ 03-back-end/bloco-29-introducao-ao-mongodb/dia-4-updates-simples/update-simples.js
@@ -25,16 +25,35 @@ db.class.updateOne({"title": "Godzilla"}, {$set: {"budget": 1}});
 
 
 // Exercício 3: Altere budget para 15 e imdbRating para 5.5 no filme Home Alone.
-
-
+db.class.updateOne({"title": "Home Alone"}, {$set: {"budget": 15, "imdbRating": 5.5}});
 
 
 // Exercício 4: Aumente em 2 o imdbRating do filme Batman.
+db.class.updateOne({"title": "Batman"}, { $inc: {"budget": 5}});
+
+
 // Exercício 5: Aumente em 5 o budget do filme Home Alone.
+db.class.updateOne({"title": "Home Alone"}, { $inc: {"budget": 5}});
+
+
 // Exercício 6: Multiplique por 4 o imdbRating do filme Batman.
+db.class.updateOne({"title": "Batman"}, { $mul: {"imdbRating": 5}});
+db.class.find({"title": "Batman"});
+
+
 // Exercício 7: Renomeie o campo budget para estimatedBudget do filme Batman.
+db.class.updateOne({"title": "Batman"}, { $rename: { budget: "estimatedBudget"}});
+
+
 // Exercício 8: Utilize o operador $min para alterar o budget para 5 do filme Home Alone.
-// Exercício 9: Utilize o operador $max para alterar o imdbRating para 8.6 do filme Godzilla. Além disso, altere a categoria "adventure" para "thriller" do filme Godzilla.
+db.class.updateOne({"title": "Home Alone"}, { $min: { budget: 5} });
+db.class.find({"title": "Home Alone"});
+
+// Exercício 9: Utilize o operador $max para alterar o imdbRating para 8.6 do filme Godzilla.
+//  Além disso, altere a categoria "adventure" para "thriller" do filme Godzilla.
+db.class.updateOne({"title": "Godzilla"}, { $max: {imdbRating: 8.6}, $set: { adventure: "thriller"}});
+db.class.find({"title": "Godzilla"});
+
 // Exercício 10: Utilizando o operador $currentDate, crie um campo chamado lastUpdated com o tipo timestamp no filme Home Alone.
 // Exercício 11: Utilizando uma única operação, crie um campo chamado sequels e atribua a ele o valor 0 em todos os documentos.
 // Exercício 12: Utilizando uma única operação, remova os campos budget e estimatedBudget em todos os documentos.
